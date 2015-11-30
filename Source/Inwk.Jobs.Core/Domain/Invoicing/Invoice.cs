19c46491db51303a5d3cd5e3d229e4c5cc4c6a9a
Alex Krupnov <akrupnov@gmail.com>
2015-11-30 11:26:15 +0100

    Added invoice entities

---
+++ Source/Inwk.Jobs.Core/Domain/Invoicing/Invoice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Inwk.Jobs.Core.Domain.Invoicing
+{
+    /// <summary>
+    /// Represents customer invoice
+    /// </summary>
+    public class Invoice
+    {
+        /// <summary>
+        /// Date of invoice issuing
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Simplified customer contact
+        /// </summary>
+        public String Customer { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IList<LineItem> Items { get; set; }
+
+        /// <summary>
+        /// Pre-calculated invoice total
+        /// </summary>
+        public Decimal TotalAmount { get; set; }
+    }
+}
---
+++ Source/Inwk.Jobs.Core/Domain/Invoicing/LineItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Inwk.Jobs.Core.Domain.Invoicing
+{
+    /// <summary>
+    /// Invoice line item
+    /// </summary>
+    public class LineItem
+    {
+        /// <summary>
+        /// Line item name
+        /// </summary>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// Total amount to be charged for customer
+        /// </summary>
+        public Decimal Amount { get; set; }
+
+        /// <summary>
+        /// Amount of tax
+        /// </summary>
+        public Decimal TaxAmount { get; set; }
+    }
+}

71f714a1f3927b4b0eb33a1082b053ca2949f704
Alex Krupnov <akrupnov@gmail.com>
2015-11-30 13:22:14 +0100

    Added missing files from prev. commit

---
+++ Source/Inwk.Jobs.Core/Input/IJobReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Inwk.Jobs.Core.Domain;
+
+namespace Inwk.Jobs.Core.Input
+{
+    /// <summary>
+    /// Reads jobs from a fo;e
+    /// </summary>
+    public interface IJobReader
+    {
+        /// <summary>
+        /// Reads jobs from a file
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        Job Read(String filePath);
+    }
+}
---
+++ Source/Inwk.Jobs.Core/Input/YamlJobReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Inwk.Jobs.Core.Domain;
+using YamlDotNet.Serialization;
+
+namespace Inwk.Jobs.Core.Input
+{
+    public class YamlJobReader : IJobReader
+    {
+        public Job Read(string filePath)
+        {
+            return ReadFromContent(File.ReadAllText(filePath));
+        }
+
+        protected internal Job ReadFromContent(String yamlConent)
+        {
+            var deserializer = new Deserializer();
+            return deserializer.Deserialize<Job>(new StringReader(yamlConent));
+        }
+    }
+}
